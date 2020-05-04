c0b86915e06ef22cf7f1510ed877bd325274435e
Ronny Hildebrandt <ronny.hildebrandt@avorium.de>
2020-05-04 09:12:00 +0200

    Links auf Überschriften eingebaut

--- doc/assets/softdoc.js
+++ doc/assets/softdoc.js
@@ -1,11 +1,16 @@
 var renderer = new marked.Renderer();
-renderer.code = function (code, language) {
+renderer.code = function(code, language) {
     if(language === 'mermaid')
         return '<div class="mermaid">' + code + '</div>';
     else {
         var validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
         return '<pre><code class="hljs ' + validLanguage + '">' + hljs.highlight(validLanguage, code).value + '</code></pre>';
     }
+};
+renderer.heading = function(text, level) {
+    var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+    var url = location.origin + location.pathname + location.search + '#' + escapedText;
+    return `<h${level} id="${escapedText}"><a class="anchor" href="${url}">${text}</a></h${level}>`;
 };
 marked.setOptions({ renderer: renderer });
 
@@ -32,13 +37,14 @@ async function loadMarkdown(url, targetselector, donotsetnewurl) {
     document.querySelector(targetselector).innerHTML = html;
     mermaid.init();
     handleAllLinks();
-    if (!donotsetnewurl) history.pushState(null, null, location.origin + location.pathname + '?' + url); // URL aktualisieren
+    if (!donotsetnewurl) history.pushState(null, null, location.origin + location.pathname + '?' + url + location.hash); // URL aktualisieren
 }
 
 window.addEventListener('load', async () => {
     await loadMarkdown('SIDEBAR.md', '#sidebar', true);
     if (location.search.length > 1) { // Wenn ?suburl angegeben ist, wird gleich dieses Dokument geladen
         await loadMarkdown(location.search.substring(1), '#content');
+        if (location.hash.length > 1) document.getElementById(location.hash.substring(1)).scrollIntoView(); // Mit #id zu einer Überschrift springen
     } else {
         await loadMarkdown('CONTENT.md', '#content');
     }
